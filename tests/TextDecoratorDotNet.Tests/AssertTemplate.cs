519f384a688ff2e09b06839006ce43178be11b30
smack0007 <zachary.snow@gmail.com>
2019-10-31 16:07:57 +0100

    Added first unit test.

---
+++ tests/TextDecoratorDotNet.Tests/AssertTemplate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace TextDecoratorDotNet.Tests
+{
+    public static class AssertTemplate
+    {
+        public static void Equal<T>(string expected, string template, T context)
+        {
+            var templateObj = Template.CompileAsync<T>(template).GetAwaiter().GetResult();
+
+            Assert.Equal(expected, templateObj.Run(context));
+        }
+    }
+}
---
+++ tests/TextDecoratorDotNet.Tests/GetTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TextDecoratorDotNet.Tests
+{
+    public class GetTestsContext
+    {
+        public string Name { get; set; }
+    }
+
+    public class GetTests
+    {
+        [Fact]
+        public void GetStringProperty()
+        {
+            AssertTemplate.Equal(
+                "Hello FooBar!",
+                "Hello @Name!",
+                new GetTestsContext()
+                {
+                    Name = "FooBar",
+                });
+        }
+    }
+}
